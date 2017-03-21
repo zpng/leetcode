fd6c32923924411135b4d12f4be3e2881091d7a4
zhangpeng <zhangpeng@fenbi.com>
2017-03-21 20:07:28 +0800

    Init commit

---
+++ src/main/java/TwoSum.java
@@ -0,0 +1,16 @@
+/**
+ * @(#)TwoSum.java, 三月 21, 2017.
+ * <p>
+ * Copyright 2017 fenbi.com. All rights reserved.
+ * FENBI.COM PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+/**
+ * @author zhangpeng
+ */
+public class TwoSum {
+
+    public int[] twoSum(int[] nums, int target) {
+        return null;
+    }
+}

672483156f346758c5b348c4eea7d9300338bcf7
zhangpeng <zhangpeng@fenbi.com>
2017-03-22 19:37:39 +0800

    ADD: question

---
+++ src/main/java/LongestPalindrome.java
@@ -0,0 +1,18 @@
+/**
+ * @(#)LongestPalindrome.java, 三月 22, 2017.
+ * <p>
+ * Copyright 2017 fenbi.com. All rights reserved.
+ * FENBI.COM PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+/**
+ * @author zhangpeng
+ */
+public class LongestPalindrome {
+
+    public String longestPalindrome(String s) {
+
+    }
+}
+
+

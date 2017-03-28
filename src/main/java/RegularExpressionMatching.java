fd7f41e7d6b27ed5d6ab6b59de829be63d10ba03
zhangpeng <zhangpeng@fenbi.com>
2017-03-28 18:43:13 +0800

    MOD: format code

--- src/main/java/RegularExpressionMatching.java
+++ src/main/java/RegularExpressionMatching.java
@@ -1,4 +1,5 @@
-/** * @(#)RegularExpressionMatching.java, 三月 26, 2017.
+/**
+ * @(#)RegularExpressionMatching.java, 三月 26, 2017.
  * <p>
  * Copyright 2017 fenbi.com. All rights reserved.
  * FENBI.COM PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
@@ -11,28 +12,28 @@ public class RegularExpressionMatching {
 
     public boolean isMatch(String s, String p) {
 
-        if (s == null || p == null){
+        if (s == null || p == null) {
             return false;
         }
-        boolean[][] state = new boolean[s.length()+1][p.length()+1];
+        boolean[][] state = new boolean[s.length() + 1][p.length() + 1];
         state[0][0] = true;
-        for (int j=1;j<state[0].length;j++){
-            if (p.charAt(j -1) == '*'){
-                if (state[0][j-1] || (j > 1 && state[0][j-2]))
-                state[0][j] = true;
+        for (int j = 1; j < state[0].length; j++) {
+            if (p.charAt(j - 1) == '*') {
+                if (state[0][j - 1] || (j > 1 && state[0][j - 2]))
+                    state[0][j] = true;
             }
         }
 
-        for (int i=1;i<state.length;i++){
-            for (int j=1;j< state[0].length; j++){
-                if (s.charAt(i-1) == p.charAt(j-1) || p.charAt(j-1) == '.'){
-                    state[i][j] = state[i-1][j-1];
+        for (int i = 1; i < state.length; i++) {
+            for (int j = 1; j < state[0].length; j++) {
+                if (s.charAt(i - 1) == p.charAt(j - 1) || p.charAt(j - 1) == '.') {
+                    state[i][j] = state[i - 1][j - 1];
                 }
-                if (p.charAt(j -1) == '*'){
-                    if (s.charAt(i -1)!=p.charAt(j-2) && p.charAt(j-2) != '.'){
-                        state[i][j] = state[i][j-2];
-                    }else {
-                        state[i][j] = state[i-1][j] || state[i][j-1] || state[i][j-2];
+                if (p.charAt(j - 1) == '*') {
+                    if (s.charAt(i - 1) != p.charAt(j - 2) && p.charAt(j - 2) != '.') {
+                        state[i][j] = state[i][j - 2];
+                    } else {
+                        state[i][j] = state[i - 1][j] || state[i][j - 1] || state[i][j - 2];
                     }
                 }
             }

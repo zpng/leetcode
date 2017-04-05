d1f0a3cf959cc552ec5f4da07c803628553d217b
zhangpeng <zhangpeng@fenbi.com>
2017-04-05 23:23:39 +0800

    ADD: PermuteUnique

---
+++ src/test/java/PermuteUniqueTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by zhangpeng on 2017/4/5.
+ */
+public class PermuteUniqueTest {
+
+    @Test
+    public void permuteUnique() throws Exception {
+        System.out.println(new PermuteUnique().permuteUnique(new int[]{1,1,1,2}));
+    }
+
+}
